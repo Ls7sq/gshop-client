898b7512e37639f030244709e8bcfd505f3f2e81
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-16 13:56:45 +0800

    FooterGuide router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
       <router-view></router-view>
       <FooterGuide/>
   </div>
@@ -13,5 +13,8 @@
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>
-    
+    .app
+      width 100%
+      height 100%
+      background #f5f5f5
 </style>
--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,15 +1,73 @@
 <template>
-  <div>FooterGuide</div>
+    <div class="footer_guide border-1px">
+    <span href="javascript:;" class="guide_item" :class="{on:'/msite' === $route.path}" @click="goTo('/msite')">
+      <span class="item_icon">
+        <i class="iconfont icon-waimai"></i>
+      </span>
+      <span>Menu</span>
+    </span>
+    <a href="javascript:;" class="guide_item" :class="{on:'/search' === $route.path}" @click="goTo('/search')">
+      <span class="item_icon">
+        <i class="iconfont icon-search"></i>
+      </span>
+      <span>Search</span>
+    </a>
+    <a href="javascript:;" class="guide_item" :class="{on:'/order' === $route.path}" @click="goTo('/order')">
+      <span class="item_icon">
+        <i class="iconfont icon-dingdan"></i>
+      </span>
+      <span>Order</span>
+    </a>
+    <!-- router-link 搭配 to 也可以 -->
+    <!-- <router-link href="javascript:;" class="guide_item" :class="{on:'/profile' === $route.path}" to='/profile'> -->
+    <a href="javascript:;" class="guide_item" :class="{on:'/profile' === $route.path}" @click="goTo('/profile')">
+      <span class="item_icon">
+        <i class="iconfont icon-geren"></i>
+      </span>
+      <span>My</span>
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'FooterGuide'
+    name:'FooterGuide',
+    methods: {
+        goTo(path){
+            this.$router.replace(path)
+        }
+    },
 
 }
 </script>
 
-
 <style lang="stylus" ref="stylesheet/stylus">
-    
+@import '../../common/stylus/mixins.styl'
+    .footer_guide  //footer
+        top-border-1px(#e4e4e4)
+        position fixed
+        z-index 100
+        left 0
+        right 0
+        bottom 0
+        background-color #fff
+        width 100%
+        height 50px
+        display flex
+        .guide_item
+            display flex
+            flex 1
+            text-align center
+            flex-direction column
+            align-items center
+            margin 5px
+            color #999999
+            &.on
+                color #02a774
+            span
+                font-size 12px
+                margin-top 2px
+                margin-bottom 2px
+                .iconfont
+                    font-size 22px
 </style>
