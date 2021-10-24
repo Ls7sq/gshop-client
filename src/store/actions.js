d79a80e0ff1bfefe05f4bcbe8b8148b3beeb6753
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-24 12:50:21 +0800

    ShopRatings组件2

--- src/store/actions.js
+++ src/store/actions.js
@@ -94,11 +94,13 @@ export default{
         }
     },
     //异步获取商家评价列表
-    async getShopRatings({commit}){
+    async getShopRatings({commit},callback){
         const result = await reqShopRatings()
         if (result.code === 0) {
             const ratings = result.data
             commit(RECEIVE_RATINGS,{ratings})
+            //此时数据已经更新了,通知组件
+            callback && callback()
         }
     },
     //异步获取商家商品列表
